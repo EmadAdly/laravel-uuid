6cc1ff1221fe6517a3cfbbefbf906b1db79713c9
EmadAdly <eadly@outlook.com>
2017-02-16 04:01:15 +0200

    use config file

--- src/Uuids.php
+++ src/Uuids.php
@@ -2,9 +2,11 @@
 namespace Emadadly\LaravelUuid;
 
 use Emadadly\LaravelUuid\UUIDManager;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 trait Uuids
 {
+
     /**
      * Boot function from laravel.
      */
@@ -12,7 +14,33 @@ trait Uuids
     {
         parent::boot();
         static::creating(function ($model) {
-            $model->{$model->getKeyName()} = UUIDManager::generate();
+            $model->{config('uuid.default_uuid_column')} = UUIDManager::generate();
+        });
+        static::saving(function ($model) {
+            $original_uuid = $model->getOriginal(config('uuid.default_uuid_column'));
+            if ($original_uuid !== $model->{config('uuid.default_uuid_column')}) {
+                $model->{config('uuid.default_uuid_column')} = $original_uuid;
+            }
         });
     }
+
+    /**
+     * Scope  by uuid 
+     * @param  string  uuid of the model.
+     * 
+    */
+    public function scopeUuid($query, $uuid, $first = true)
+    {
+        $match = preg_match('/^[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}$/', $uuid);
+
+        if (!is_string($uuid) || $match !== 1)
+        {
+            throw (new ModelNotFoundException)->setModel(get_class($this));
+        }
+    
+        $results = $query->where(config('uuid.default_uuid_column'), $uuid);
+    
+        return $first ? $results->firstOrFail() : $results;
+    }
+
 }
